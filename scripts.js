8b52989e16e64f5cf932c46d7ef2415e1a49e15e
Liz Keller <sazanamisama@gmail.com>
2013-03-17 12:36:24 -0400

    basically working prototype

--- scripts.js
+++ scripts.js
@@ -23,6 +23,12 @@ $(document).ready(function() {
 		$('#gpsText').show();
 	});
 
+	$('#gps #backToFindParking').click(function () {
+		$('.page').hide();
+		$('#findParking').show();
+		$('#pageTitle').html('Find Parking');
+	});
+
 	$('#gpsText .knurl').click(function() {
 		$('.page').hide();
 		$('#gps').show();
@@ -33,39 +39,121 @@ $(document).ready(function() {
 		$('#payment').show();
 		$('#pageTitle').html('Parking Options');
 		$('#links').hide();
-	})
+	});
 
 	$('#gps #park').click(function() {
 		$('.page').hide();
 		$('#payment').show();
 		$('#pageTitle').html('Parking Options');
 		$('#links').hide();
-	})
+	});
+
+	$('#payment #cancelParking').click(function () {
+		$('.page').hide();
+		$('#findParking').show();
+		$('#pageTitle').html('Find Parking');
+	});
 
 	$('.subMenu .menuOptions li').click(function() {
 		var menuItem = $(this);
 		menuItem.parent().parent().children().children().removeClass('selected');
 		menuItem.addClass('selected');
-	})
+	});
 
 	$('#addCar').click(function() {
 		$('.page').hide();
 		$('#paymentAddCar').show();
 		$('#pageTitle').html('Add Car');
 
-	})
+	});
 
 	$('#addPayment').click(function() {
 		$('.page').hide();
 		$('#paymentAddPayment').show();
 		$('#pageTitle').html('Add Payment Option');
-	})
+	});
 
 	$('#editNotifications').click(function() {
 		$('.page').hide();
 		$('#paymentEditNotifications').show();
 		$('#pageTitle').html('Edit Notifications');
-	})
+	});
 
+	$('#cancelAddCar').click(function() {
+		$('#paymentAddCar li').removeClass('selected');
+		$('#licenseNumber').val('');
+		$('.page').hide();
+		$('#payment').show();
+		$('#pageTitle').html('Parking Options');
+	});
+
+	$('#cancelAddPayment').click(function () {
+		$('.page').hide();
+		$('#payment').show();
+		$('#pageTitle').html('Parking Options');
+	});
+
+	$('#cancelEditNotifications').click(function() {
+		$('.page').hide();
+		$('#payment').show();
+		$('#pageTitle').html('Parking Options');
+	});
+
+	$('#addCarButton').click(function() {
+		var license = $('#licenseNumber').val();
+		//choose the selected color
+		//var color = $('#paymentAddCar li')
+		var color = 'blue';
+		//add the new car
+		$('#carChoices ul').append('<li><img src="img/car_' + color + '.png" width="50px"></img> ' + license + '</li>');
+
+		$('#paymentAddCar li').removeClass('selected');
+		$('#licenseNumber').val('');
+		$('.page').hide();
+		$('#payment').show();
+		$('#pageTitle').html('Parking Options');
+	});
+
+	$('#parkNow').click(function() {
+		$('.page').hide();
+		$('#parked').show();
+		$('#pageTitle').html('Something???');
+	});
+
+	$('#findCarButton').click(function () {
+		$('.page').hide();
+		$('#findCar').show();
+		$('#pageTitle').html('Route to Car');
+	});
+
+	$('#findCar .knurl').click(function () {
+		$('.page').hide();
+		$('#findCarText').show();
+		$('#pageTitle').html('Route to Car');
+	});
+
+	$('#findCarText .knurl').click(function () {
+		$('.page').hide();
+		$('#findCar').show();
+		$('#pageTitle').html('Route to Car');
+	});
+
+	$('#doneParkingButton').click(function() {
+		$('.page').hide();
+		$('#doneParking').show();
+		$('#pageTitle').html('Thanks come again');
+	});
+
+	$('#doneFinding').click(function() {
+		$('.page').hide();
+		$('#doneParking').show();
+		$('#pageTitle').html('Thanks come again');
+	});
+
+	$('#backToParked').click(function() {
+		$('.page').hide();
+		$('#parked').show();
+		$('#pageTitle').html('Something???');
+	});
 
 });
